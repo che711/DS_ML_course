af7b43cd41657e48e05efc31ee471f69276e3fc7
che711 <che711@mail.ru>
2024-06-17 21:11:31 +0200

    updated acording to remote branch

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -1,1 +1,8 @@
 print('my name is {myname} and my phone number {phone_number}'.format(myname="Tonny", phone_number=3333333))
+
+my_list=['жизнь', 'cloud', 8, 'string', 234]
+data_1=my_list[-2]
+
+print(f"Full list: {my_list}")
+print(f"data_1: {data_1}")
+
