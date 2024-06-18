d412ff71ea02c1f79942409a914a6c835878acd7
che711 <che711@mail.ru>
2024-06-18 08:14:49 +0200

    updated lesson1.py

---
+++ lesson1 > python/lesson2.py
@@ -0,0 +1,13 @@
+a = 1
+b = 2
+
+print(f"Value of a > b: {a > b}")
+
+print(f'Value of "f" == "f": {"f" == "f"}')
+
+print(f"Value of (1 == 3)or('a'=='a'): {(1 == 3)or('a'=='a')}")
+
+
+if 1 == 2: print("Done")
+elif 3 == 3: print("The second condition")
+else: print("Not done")
