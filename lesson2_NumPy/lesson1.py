29266ecd68c314c0c5752072158aec8dc9b75bef
che711 <che711@mail.ru>
2024-06-24 08:02:15 +0200

    added exam data

---
+++ lesson2_NumPy/lesson1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+mylist = [1, 2, 3, 4, 5, 6, 7, 8]
+print(f"The type of data my_list: {type(mylist)}")
+
+myarr = np.array(mylist)
+print(f"myarr: {myarr}")
+
+# одномерный массив
+myarr = np.array(mylist)
+print(f"Просто переменная marr: {myarr}.\nТип данных myarr: {type(myarr)}")
+
+
+
+
+
+
+
+
+
+
