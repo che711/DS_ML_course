21850dc7aa96db1b6f173e758b3e900938aaeb5c
che711 <che711@mail.ru>
2024-06-27 07:13:34 +0200

    updated lesson2.py

--- lesson2_NumPy/lesson2.py
+++ lesson2_NumPy/lesson2.py
@@ -2,9 +2,9 @@ import numpy as np
 
 
 arr = np.arange(0, 11)
-print(f"Array from 0 to 11: {arr}")
-print(f"Position 8 from the list above: {arr[8]}")
-print(f"Range from 3 to 7 in the list above: {arr[3:7]}")
+print(f"Array from 0 to 11 ->K {arr}")
+print(f"Position 8 from the list above -> {arr[8]}")
+print(f"Range from 3 to 7 in the list above -> {arr[3:7]}")
 print(f"Range :5 -> {arr[:5]}")
 print(f"Range 7: -> {arr[7:]}")
 
