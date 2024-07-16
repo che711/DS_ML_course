bd212934f18be1f080c5b5595cd580bebc67ddb2
che711 <che711@mail.ru>
2024-07-16 08:44:22 +0200

    updated lessons 3

--- lesson3_Pandas/lesson3_filtering_by_condition.py
+++ lesson3_Pandas/lesson3_filtering_by_condition.py
@@ -3,3 +3,23 @@ import pandas as pd
 
 df = pd.read_csv('../course_data/03-Pandas/tips.csv')
 print(df)
+
+# выбираем строки в колонке по условию
+bills_more_than_40 = df['total_bill'] >= 40
+print(bills_more_than_40)
+
+# Выбираем мужчин из таблицы
+men = df['sex'] != 'Female'
+print(f'Выбираем мужчин из таблицы: \n{men}')
+
+print(df[df['sex'] != 'Female'])
+
+# Выбираем строки с воскресеньем
+sunday = [df['day'] == 'Sun']
+print(f"выбираем воскресенье: \n{sunday}")
+
+
+
+
+
+
