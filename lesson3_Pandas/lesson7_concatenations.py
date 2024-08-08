d07a900cbd52931ba71bdfce516a70c1e4294f2c
che711 <che7111987@gmail.com>
2024-08-08 08:01:33 +0200

    updated lessons7

--- lesson3_Pandas/lesson7_concatenations.py
+++ lesson3_Pandas/lesson7_concatenations.py
@@ -1,6 +1,16 @@
 import numpy as np
 import pandas as pd
 
+data_one = {'A': ['A0', 'A1', 'A2', 'A3'], 'B': ['B0', 'B1', 'B2', 'B3']}
+data_two = {'C': ['C0', 'C1', 'C2', 'C3'], 'D': ['D0', 'D1', 'D2', 'D3']}
 
+one = pd.DataFrame(data_one)
+two = pd.DataFrame(data_two)
+
+print(f'\n -> \n{one}')
+print(f'\n -> \n{one}')
+
+
+# print(f'\n -> \n{}')
 
 
