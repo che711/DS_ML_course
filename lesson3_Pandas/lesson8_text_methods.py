c5815df285195604c18bb91ef32f9eb4ca6f6ce0
che711 <che7111987@gmail.com>
2024-08-14 08:15:44 +0200

    updated lessons8

--- lesson3_Pandas/lesson8_text_methods.py
+++ lesson3_Pandas/lesson8_text_methods.py
@@ -1,5 +1,18 @@
 import numpy as np
 import pandas as pd
 
+email = 'vlad@email.com'
+print(f"email.split('@')")
+
+names = pd. Series(['andrew', 'bobo', 'claire', 'david', '5'])
+print(f"\n -> \n{names}")
+
+print(f"\n -> \n{names.str.upper()}")
+print(f"\n -> \n{names.str.lower()}")
+
+# print(f"\n -> \n{}")
+# print(f"\n -> \n{}")
+# print(f"\n -> \n{}")
+
 
 
