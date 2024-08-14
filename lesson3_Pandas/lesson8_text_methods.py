8ef385bce058673632898163f9f38a7cf287cc9d
che711 <che7111987@gmail.com>
2024-08-14 08:30:05 +0200

    updated lessons8

--- lesson3_Pandas/lesson8_text_methods.py
+++ lesson3_Pandas/lesson8_text_methods.py
@@ -10,9 +10,30 @@ print(f"\n -> \n{names}")
 print(f"\n -> \n{names.str.upper()}")
 print(f"\n -> \n{names.str.lower()}")
 
-# print(f"\n -> \n{}")
-# print(f"\n -> \n{}")
-# print(f"\n -> \n{}")
+tech_finance = ['GOOG,APPL,AMZN', 'JPN, BAC,GS']
+len(tech_finance)
+print(f"\n -> \n{len(tech_finance)}")
+
+tech = 'GOOG,APPL,AMZN'
+print(f"\n -> \n{tech.split(',')[0]}")
+
+tickers = pd.Series(tech_finance)
+print(f"\n -> \n{tickers.str.split(',', expand=True)}")
+print(f"\n -> \n{tickers.str.split(',')}")
+print(f"\n -> \n{tickers.str.split(',').str[0]}")
+
+messy_names = pd.Series(['andrew  ', 'bo:bo', '  clair  '])
+print(f"\n -> \n{messy_names}")
+print(f"\n -> \n{messy_names.str.replace(':', '').str.strip().str.capitalize()}")
+
+def cleanup(name):
+    name = name.replace(':', '')
+    name = name.strip()
+    name = name.capitalize()
+    return name
+
+
+print(f"\nприменяем ф-цию ко всему объекту pd.Series: -> \n{messy_names.apply(cleanup)}")
 
 
 
